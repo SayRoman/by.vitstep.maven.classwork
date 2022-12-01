377d89855a56a0606ad479e3c3829e8cc1ef5718
olga1324 <106828262+olga1324@users.noreply.github.com>
2022-12-01 20:14:57 +0300

    added new files

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -3,12 +3,28 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class TestClass extends BaseTestClass{
+public class TestClass extends BaseTestClass {
 
     @Test
-    public void testMethod(){
+    public void testMethod() {
         readProperties();
         String url = System.getProperty("base-url");
         Assertions.assertEquals("http:\\\\google.com", url);
     }
+
+
+    @Test
+    public void testMethod2() {
+        readProperties();
+        String username = System.getProperty("user-name");
+        Assertions.assertEquals("user", username);
+    }
+
+    @Test
+    public void testMethod3() {
+        readProperties();
+        String userlastname = System.getProperty("user-lastname");
+        Assertions.assertEquals("lastname", userlastname);
+    }
+
 }
